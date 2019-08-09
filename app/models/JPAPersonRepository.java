eb62740fa218c37f993c739b027775580331e0ee
DerKaiserAtora <53656746+atrariksa@users.noreply.github.com>
2019-08-09 23:00:32 +0700

    Add_Minimum_Database_Requirement

---
+++ app/models/JPAPersonRepository.java
@@ -0,0 +1,71 @@
+package models;
+
+import play.db.jpa.JPAApi;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+/**
+ * Provide JPA operations running inside of a thread pool sized to the connection pool
+ */
+public class JPAPersonRepository implements PersonRepository {
+
+    private final JPAApi jpaApi;
+    private final DatabaseExecutionContext executionContext;
+
+    @Inject
+    public JPAPersonRepository(JPAApi jpaApi, DatabaseExecutionContext executionContext) {
+        this.jpaApi = jpaApi;
+        this.executionContext = executionContext;
+    }
+
+    @Override
+    public CompletionStage<Person> add(Person person) {
+        return supplyAsync(() -> wrap(em -> insert(em, person)), executionContext);
+    }
+
+    @Override
+    public CompletionStage<Stream<Person>> list() {
+        return supplyAsync(() -> wrap(em -> list(em)), executionContext);
+    }
+
+    @Override
+    public CompletionStage<Person> remove(Person person) {
+        return supplyAsync(() -> wrap(em -> delete(em, person)), executionContext);
+    }
+
+    private Person delete(EntityManager em, Person person) {
+        em.remove(em.contains(person) ? person : em.merge(person));
+        return person;
+    }
+
+    private <T> T wrap(Function<EntityManager, T> function) {
+        return jpaApi.withTransaction(function);
+    }
+
+    private Person insert(EntityManager em, Person person) {
+        em.persist(person);
+        return person;
+    }
+
+    private Stream<Person> list(EntityManager em) {
+        List<Person> persons = em.createQuery("select p from Person p", Person.class).getResultList();
+        return persons.stream();
+    }
+
+//    @Override
+//    public CompletionStage<Person> getPerson(int id) {
+//        return supplyAsync(() -> wrap(em -> getPersonById(em, id)), executionContext);
+//    }
+
+    public Person getPersonById (int id) {
+        Person person = (Person) jpaApi.em("default").createQuery("select p from Person p where p.id = :idPerson").setParameter("idPerson",Long.valueOf(id)).getSingleResult();
+        return person;
+    }
+}
